70f6d0d1d0776511e23d14c256f5119c808b78a5
Samir Hammoudi <ksimir@gmail.com>
2017-09-13 17:32:56 +0900

    Update main.go

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Spinnaker is so cool!!!")
+	io.WriteString(w, "Spinnaker is SUPER cool!!!")
 }
 
 func main() {
